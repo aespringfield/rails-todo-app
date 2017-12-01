3cb417bfe036bd8ace1f4eb763f42a40b6856c83
Y Paul Sussman <ypaulsussman@gmail.com>
2017-11-30 20:38:36 -0600

    begins create route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,24 @@
 class TasksController < ApplicationController
+  def index
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:notice] = "Wooo! New Task!!!!"
+      redirect_to @task
+    else
+      # nothing, yet
+    end
+  end
+
+  private
+  def task_params
+    params
+  end
+
 end
